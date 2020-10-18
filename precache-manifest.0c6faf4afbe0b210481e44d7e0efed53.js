1ce7e9e51d8a7fc3baa9ba73594b92b8c40612ea
Maciej Dabrowski <dabrowskimaciej@protonmail.com>
2020-10-18 18:18:24 +0200

    Updates

---
+++ precache-manifest.0c6faf4afbe0b210481e44d7e0efed53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62541f9f3832e9388ca1cff7bff663e8",
+    "url": "/goodtimer/index.html"
+  },
+  {
+    "revision": "7af3ff98632b5ee7a923",
+    "url": "/goodtimer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eeb3875789ec2b28af19",
+    "url": "/goodtimer/static/js/2.8cfe1b50.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/goodtimer/static/js/2.8cfe1b50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7af3ff98632b5ee7a923",
+    "url": "/goodtimer/static/js/main.f534a322.chunk.js"
+  },
+  {
+    "revision": "2394f360f7a6e90e192b",
+    "url": "/goodtimer/static/js/runtime-main.a9009dd1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/goodtimer/static/media/logo.5d5d9eef.svg"
+  }
+]);
